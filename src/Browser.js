22b8442d350a1e036805eddcdba2c5772c9d0216
Anita Ambalavanan <t-anamba@microsoft.com>
2020-06-28 11:02:38 -0400

    browser page skeleton

--- src/Browser.js
+++ src/Browser.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import './Browser.css';
+
+const list = [
+    {
+
+      name: 'Lifting Black Voices and Authors',
+      topic: 'Black Experience in America',
+      leader: 'Jane Doe',
+      startDate: '6/28/2020',
+      endDate: '7/20/2020'
+    },
+    {
+
+        name: 'Native American Traditions and History',
+        topic: 'Indigenous Culture in North America',
+        leader: 'Jane Doe',
+        start: '4/30/2020',
+        endDate: '5/29/2020'
+    }
+  ];
  
+const nestedLists = [list];
+
 const browser = () => {
     return (
+
        <div>
-          <h1>Browse</h1>
-           <p>Browse bookclubs </p>
+           <div>
+               <h1>Browse Bookclubs</h1>
+           </div>
+         <ul>
+         {nestedLists.map((nestedList, index) => (
+           <ul key={index}>
+             {nestedList.map(item => (
+               <li key={item.name}>
+                 <div>{item.name}</div>
+                 <div>Topic: {item.topic}</div>
+                 <div>Leader: {item.leader}</div>
+                 <div>Start Date: {item.startDate}</div>
+                 <div>End Date: {item.endDate}</div>
+               </li>
+             ))}
+           </ul>
+         ))}
+       </ul>
        </div>
     );
 }
